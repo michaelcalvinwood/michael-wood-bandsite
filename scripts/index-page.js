a1024e107ca0d195ac5e10582ea0333f294821d3
Michael Wood <michaelwood33311@icloud.com>
2022-02-19 13:48:20 -0500

    refactored code to remove test variable and work directly on the timestamp parameter

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,38 +1,7 @@
 const apiKey = "f3bd5bed-c466-449a-bc93-1d14dc589719";
 const baseUrl = "https://project-1-api.herokuapp.com/";
 
-/*
- * createElement: Creates a child element and appends to parent
- * @param parent (Element): parent element to attach child to
- * @param tag (String): element tag to create
- * @param c (String): optional class(es) of the element. Multiple classes are allowed when separated by a space.
- * @param text (String): optional innerText of the element
- * @param attributes (Obj): optional object whose key/value pairs will be added to the element
- * @return Element on success (the created child element); false on error (if tag contains invalid characters). 
- */
- 
-function createElement (parent, tag, c = false, text = false, attributes = false) {
-    let el = {};
-    try {
-        el = document.createElement(tag);
-    } catch (e) {
-        return false;
-    }
-
-    parent.appendChild(el);
-    
-    // optional parameters
-    if (c) el.className = c; 
-    if (text) el.innerText = text;
-    if (attributes) {
-        for (const [key, value] of Object.entries(attributes)) {
-            el.setAttribute (key, value);
-        };
-    }
-
-    return el
-}
-
+XMLDocument
 
 
 /*
@@ -116,16 +85,6 @@ function updateSubmittedComments (submittedCommentsSection) {
  * @return String: formatted output to display the comment time
  */
 function displayLiveDate (timestamp) {
-    let test;
-
-    // if (timestamp.indexOf('/') !== -1) {
-    //     const dateParts = timestamp.split('/');
-    //     const dateString = `${dateParts[2]}-${dateParts[0]}-${dateParts[1]}`;
-    //     test = Date.parse(dateString)/1000;
-    //     if (isNaN(test)) return timestamp;
-    //     timestamp = test;
-    // }
-
     timestamp = Number(timestamp);
 
     const currentTime = Date.now();
